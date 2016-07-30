2597effa25ad0a0410ef1b996c1bcffba31e6a57
Ryan Gomoto <ryan@cyrusbio.com>
2016-07-29 18:01:56 -0700

    Install sequence-viewer

---
+++ src/Data.ts
@@ -0,0 +1,7 @@
+import GridData from 'sequence-viewer/lib/GridData';
+
+class Data implements GridData {
+
+}
+
+export default Data;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-console.log('Hello, world!');
+import Grid from 'sequence-viewer/lib/Grid';
+
+console.log('Hello, world!', Grid);
